d8bf87398edc0bfaffa3d237a288b8dac424d349
Jasmin Dzogovic <jasminxhogovic@gmail.com>
2023-10-25 12:35:09 -0400

    added programmatic navigation to the map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,17 +1,26 @@
-import { useParams, useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./map.module.css";
 
 function Map() {
+  const navigate = useNavigate();
+
   const [searchParams, setSearchParams] = useSearchParams();
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
 
   return (
-    <div className={styles.mapContainer}>
+    <div
+      className={styles.mapContainer}
+      onClick={() => {
+        navigate("form");
+      }}
+    >
       <h1>Map</h1>
       <span>Position: {lat}, </span>
       <span>{lng}</span>
-      <button onClick={()=> setSearchParams({lat: 23, lng: 50})}>Change Position</button>
+      <button onClick={() => setSearchParams({ lat: 23, lng: 50 })}>
+        Change Position
+      </button>
     </div>
   );
 }
